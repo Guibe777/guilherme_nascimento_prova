0db1b6ca2ca4591f0305a0889601f00f6ce6c05c
Guilherme Do Nascimento <guilhermedonascimento157@gmail.com>
2025-08-22 16:39:33 -0300

    Processa alteração de funcionário
    Agora está funcionando, linhas de código inúteis excluídas e mudanças no código para ele funcionar, além de comentários

--- processa_alteracao_funcionario.php
+++ processa_alteracao_funcionario.php
@@ -2,35 +2,33 @@
     session_start();
     require_once 'conexao.php';
 
+    // Verificando se o usuário é ADM
     if($_SESSION['perfil'] != 1) {
         echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
         exit();
     }
 
+    // Pegando as informações do funcionário
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $id_funcionario = $_POST['id_funcionario'];
         $nome = $_POST['nome_funcionario'];
         $email = $_POST['email'];
-       // $id_perfil = $_POST['id_perfil'];
-       // $nova_senha = !empty($_POST['nova_senha'])? password_hash($_POST['nova_senha'],PASSWORD_DEFAULT): null;
+        $endereco = $_POST['endereco'];
+        $telefone = $_POST['telefone'];
 
-        // Atualiza os dados do usuário
-        /* if($nova_senha) {
-            $sql = "UPDATE funcionario SET nome_funcionario=:nome_funcionario,email=:email
-            WHERE id_funcionario = :id ";
-            $stmt = $pdo->prepare($sql);
-           // $stmt->bindParam(':senha', $nova_senha);
-        } */
-        if ($id_funcionario) {
-            $sql = "UPDATE funcionario SET nome_funcionario = :nome_funcionario, email = :email
+        // Atualiza os dados do funcionário
+            $sql = "UPDATE funcionario SET nome_funcionario = :nome_funcionario, email = :email, endereco = :endereco,
+            telefone = :telefone
             WHERE id_funcionario = :id";
             $stmt = $pdo->prepare($sql);
-        }
+
         $stmt->bindParam(':nome_funcionario', $nome);
         $stmt->bindParam(':email', $email);
-        // $stmt->bindParam(':id_perfil', $id_perfil);
-        $stmt->bindParam(':id_funcionario', $id_funcionario);
+        $stmt->bindParam(':endereco', $endereco);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':id', $id_funcionario);
 
+        // Mensagens
         if($stmt->execute()) {
             echo "<script>alert('Funcionário atualizado com sucesso!');window.location.href='buscar_funcionario.php';</script>";
         }
